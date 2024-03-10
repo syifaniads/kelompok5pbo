8b735bd70819225eaf06f6a8881134374203582c
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-03-10 15:12:16 +0700

    Update user.java

--- FilkomTravel/filkomtravel/src/user.java
+++ FilkomTravel/filkomtravel/src/user.java
@@ -7,16 +7,16 @@ public class user {
     user member = new user();
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        System.out.print("\n"+"=".repeat(52));
+        System.out.print("=".repeat(52));
         System.out.print("\n"+"=".repeat(15));
         System.out.print("APLIKASI FILKOM TRAVEL");
         System.out.print("=".repeat(15));
         System.out.print("\n"+"=".repeat(52));
-        System.out.print("\n SIAPA ANDA?");
-        System.out.print("\n Ketik 1 Daftar Member");
-        System.out.print("\n Ketik 2 Daftar non-Member");
-        System.out.print("\n Ketik 3 Daftar Penyewa");
-        System.out.print("\n PILIH MASUKKAN: ");
+        System.out.print("\nSIAPA ANDA?");
+        System.out.print("\nKetik 1 Daftar Member");
+        System.out.print("\nKetik 2 Daftar non-Member");
+        System.out.print("\nKetik 3 Daftar Penyewa");
+        System.out.print("\nPILIH MASUKKAN: ");
         int choice = scanner.nextInt();
         scanner.nextLine();
         switch(choice){
@@ -34,7 +34,25 @@ public class user {
                 System.out.println("SELAMAT ANDA BERHASIL MENJADI MEMBER!");
                 System.out.print("=".repeat(52));
                 System.out.println("\nLANJUTKAN PESANAN?");
-                // if else?
+                System.out.println("1. Ya");
+                System.out.println("2. Tidak");
+                System.out.print("Masukkan pilihan: ");
+                int pilihan1 = scanner.nextInt();
+                scanner.nextLine();
+                if(pilihan1==1){
+                    System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
+                    String tanggalAwal = scanner.nextLine();
+                    System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
+                    String tanggalAkhir = scanner.nextLine();
+                    System.out.print("Durasi penyewaan (dalam hari): ");
+                    int durasi = scanner.nextInt();
+                    scanner.nextLine();
+                    // panggil class nota
+                }
+                else{
+                    System.out.println("Terimakasih!");
+                    break;
+                }
                 break;
             case 2:
                 System.out.print("Masukkan nama: ");
@@ -46,7 +64,25 @@ public class user {
                 System.out.println("SELAMAT ANDA BERHASIL MENDAFTAR!");
                 System.out.print("=".repeat(52));
                 System.out.println("\nLANJUTKAN PESANAN?");
-                // if else?
+                System.out.println("1. Ya");
+                System.out.println("2. Tidak");
+                System.out.print("Masukkan pilihan: ");
+                int pilihan2 = scanner.nextInt();
+                scanner.nextLine();
+                if(pilihan2==1){
+                    System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
+                    String tanggalAwal = scanner.nextLine();
+                    System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
+                    String tanggalAkhir = scanner.nextLine();
+                    System.out.print("Durasi penyewaan (dalam hari): ");
+                    int durasi = scanner.nextInt();
+                    scanner.nextLine();
+                    // panggil class nota
+                }
+                else{
+                    System.out.println("Terimakasih!");
+                    break;
+                }
                 break;
             case 3:
                 System.out.print("Masukkan nama: ");
@@ -60,11 +96,14 @@ public class user {
 //                System.out.print("Masukkan Jenis Kendaraan: ");
 //                String jenisVehiclePenyewa = scanner.nextLine();
                 // ini mereka yang input atau dari kita???
-                
+
                 System.out.println("SELAMAT ANDA BERHASIL MENJADI PENYEWA!");
                 System.out.print("\n"+"=".repeat(52));
                 System.out.println("LANJUTKAN PESANAN?");
                 // if else?
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
                 break;
         }
     }
