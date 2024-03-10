6309284115eefb346d576fe67cccbaa2a5fcfe15
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-03-10 22:13:11 +0700

    NonMember.java

--- FilkomTravel/filkomtravel/src/NonMember.java
+++ FilkomTravel/filkomtravel/src/NonMember.java
@@ -1,29 +1,72 @@
 import java.util.*;
 public class NonMember extends user {
     Scanner scanner = new Scanner(System.in);
+    private String username;
+    private String password;
     private String tanggalAwalSewa;
     private String tanggalAkhirSewa;
     private int durasiSewa;
     private double hargaSewa;
 
+    public void setTanggalAwalSewa(String s){
+        tanggalAkhirSewa = s;
+    }
+    public void setTanggalAkhirSewa(String s){
+        tanggalAkhirSewa = s;
+    }
+    public void setDurasiSewa(int durasiSewa) {
+        this.durasiSewa = durasiSewa;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public void jadiMember(){
+        Member member = new Member();
+        System.out.print("Masukkan Nama: ");
+        member.setNama(scanner.nextLine());
+        System.out.print("Masukkan Nomor Telepon: ");
+        member.setNoHP(scanner.nextLine());
+        System.out.print("Masukan Alamat: ");
+        member.setAlamat(scanner.nextLine());
+        System.out.print("Masukkan username yang diinginkan: ");
+        member.setUsername(scanner.nextLine());
+        System.out.print("Masukkan password yang sulit: ");
+        member.setUsername(scanner.nextLine());
+        System.out.println("SELAMAT ANDA BERHASIL MENJADI MEMBER!");
+    }
     public void buatPesanan() {
+        Member member = new Member();
+        System.out.println("Buat Pesanan:");
         System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
-        String tanggalAwal = scanner.nextLine();
+        member.setTanggalAwalSewa(scanner.nextLine());
         System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
-        String tanggalAkhir = scanner.nextLine();
+        member.setTanggalAkhirSewa(scanner.nextLine());
         System.out.print("Durasi penyewaan (dalam hari): ");
-        int durasi = scanner.nextInt();
+        member.setDurasiSewa(scanner.nextInt());
         scanner.nextLine();
     }
+
     public void editPesanan() {
+        Member member = new Member();
+        System.out.println("Edit Pesanan:");
         System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
-        String tanggalAwal = scanner.nextLine();
+        member.setTanggalAwalSewa(scanner.nextLine());
         System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
-        String tanggalAkhir = scanner.nextLine();
+        member.setTanggalAkhirSewa(scanner.nextLine());
         System.out.print("Durasi penyewaan (dalam hari): ");
-        int durasi = scanner.nextInt();
+        member.setDurasiSewa(scanner.nextInt());
         scanner.nextLine();
     }
+
     public void batalkanPesanan() {
         System.out.println("Batalkan Pesanan:");
         // Implementasi logika pembatalan pesanan
@@ -31,6 +74,5 @@ public class NonMember extends user {
         tanggalAkhirSewa = null;
         durasiSewa = 0;
         hargaSewa = 0.0;
-        System.out.println("Pesanan berhasil dibatalkan.");
     }
 }
