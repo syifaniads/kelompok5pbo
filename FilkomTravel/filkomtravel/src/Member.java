235fc4c8f40dff1cab61736152e55db80fc913f3
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-03-10 19:23:46 +0700

    Update Member.java

--- FilkomTravel/filkomtravel/src/Member.java
+++ FilkomTravel/filkomtravel/src/Member.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Member extends user {
+    Scanner scanner = new Scanner(System.in);
     private String username;
     private String password;
     private double voucherMember;
@@ -8,8 +9,19 @@ public class Member extends user {
     private int durasiSewa;
     private double hargaSewa;
 
+    public void setTanggalAwalSewa(){
+        // blm tau
+    }
+
+
     public void buatPesanan() {
-        // implementasi
+        System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
+        String tanggalAwal = scanner.nextLine();
+        System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
+        String tanggalAkhir = scanner.nextLine();
+        System.out.print("Durasi penyewaan (dalam hari): ");
+        int durasi = scanner.nextInt();
+        scanner.nextLine();
     }
 
     public void editPesanan() {
