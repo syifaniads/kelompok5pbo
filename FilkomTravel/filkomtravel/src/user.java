25c99f2180b9c6ff56be29150c8ef12f41da5218
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-03-10 13:52:00 +0700

    Update user.java
    menu awal udah selesai macamnya

--- FilkomTravel/filkomtravel/src/user.java
+++ FilkomTravel/filkomtravel/src/user.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.util.Scanner;
-
 public class user {
     String nama;
     String noHP;
@@ -20,22 +18,54 @@ public class user {
         System.out.print("\n Ketik 3 Daftar Penyewa");
         System.out.print("\n PILIH MASUKKAN: ");
         int choice = scanner.nextInt();
+        scanner.nextLine();
         switch(choice){
             case 1:
-            System.out.println("Masukkan nama: ");
-            String nama = scanner.nextLine();
-            System.out.print("Masukkan Nomor Telepon: ");
-            String noTelp= scanner.nextLine();
-            System.out.print("Masukan Alamat: ");
-            String alamat= scanner.nextLine();
-            System.out.print("Masukkan username yang diinginkan: ");
-            String username=scanner.nextLine();
-            System.out.print("Masukkan password yang sulit: ");
-            String password=scanner.nextLine();
-            System.out.println("SELAMAT ANDA BERHASIL MENJADI MEMBER!");
-            System.out.print("\n"+"=".repeat(52));
-            System.out.println("LANJUTKAN PESANAN?");
+                System.out.print("Masukkan Nama: ");
+                String nama = scanner.nextLine();
+                System.out.print("Masukkan Nomor Telepon: ");
+                String noTelp = scanner.nextLine();
+                System.out.print("Masukan Alamat: ");
+                String alamat = scanner.nextLine();
+                System.out.print("Masukkan username yang diinginkan: ");
+                String username = scanner.nextLine();
+                System.out.print("Masukkan password yang sulit: ");
+                String password = scanner.nextLine();
+                System.out.println("SELAMAT ANDA BERHASIL MENJADI MEMBER!");
+                System.out.print("=".repeat(52));
+                System.out.println("\nLANJUTKAN PESANAN?");
+                // if else?
+                break;
+            case 2:
+                System.out.print("Masukkan nama: ");
+                String namaNonMember = scanner.nextLine();
+                System.out.print("Masukkan Nomor Telepon: ");
+                String noTelpNon = scanner.nextLine();
+                System.out.print("Masukkan Alamat: ");
+                String alamatNonMember = scanner.nextLine();
+                System.out.println("SELAMAT ANDA BERHASIL MENDAFTAR!");
+                System.out.print("=".repeat(52));
+                System.out.println("\nLANJUTKAN PESANAN?");
+                // if else?
+                break;
+            case 3:
+                System.out.print("Masukkan nama: ");
+                String namaPenyewa = scanner.nextLine();
+                System.out.print("Masukkan Nomor Telepon: ");
+                String noTelpPenyewa = scanner.nextLine();
+                System.out.print("Masukkan Alamat: ");
+                String alamatPenyewa = scanner.nextLine();
+//                System.out.print("Masukkan Nomor Polisi Kendaraan: ");
+//                String noPolPenyewa = scanner.nextLine();
+//                System.out.print("Masukkan Jenis Kendaraan: ");
+//                String jenisVehiclePenyewa = scanner.nextLine();
+                // ini mereka yang input atau dari kita???
+                
+                System.out.println("SELAMAT ANDA BERHASIL MENJADI PENYEWA!");
+                System.out.print("\n"+"=".repeat(52));
+                System.out.println("LANJUTKAN PESANAN?");
+                // if else?
+                break;
         }
     }
 }
-
