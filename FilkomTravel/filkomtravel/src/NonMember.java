bb414db7ed6707261a0833404ee559e9ae03cabd
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-03-10 18:20:32 +0700

    Create NonMember.java

---
+++ FilkomTravel/filkomtravel/src/NonMember.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class NonMember extends user {
+    private Date tanggalAwalSewa;
+    private Date tanggalAkhirSewa;
+    private int durasiSewa;
+    private double hargaSewa;
+
+    public void buatPesanan() {
+        // implementasi
+    }
+    public void editPesanan() {
+        // implementasi
+    }
+    public void batalkanPesanan() {
+        // implementasi
+    }
+}
