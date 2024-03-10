0ad61794bf19ec637fe3a099e48133fc7fe6dc01
mauliasugeha <162555141+mauliasugeha@users.noreply.github.com>
2024-03-10 16:20:09 +0700

    kendaraan.java

--- FilkomTravel/filkomtravel/src/kendaraan.java
+++ FilkomTravel/filkomtravel/src/kendaraan.java
@@ -1,24 +1,39 @@
 // bener gak yuh
-public class Kendaraan {
-    String noPol;
-    double hargaSewa;
-    String jenisKendaraan;
-    int kapasitas;
-    boolean statusKendaraan;
+public class kendaraan {
+    protected String noPol;
+    protected String jenisKendaraan;
+    protected int kapasitas;
+    protected double hargaSewa;
 
-    public void setStatusKendaraan(boolean statusKendaraan) {
+    protected boolean statusKendaraan;
+
+    public kendaraan(String jenisKendaraan, String noPol, int kapasitas, double hargaSewa, boolean statusKendaraan){
+        this.jenisKendaraan = jenisKendaraan;
+        this.noPol = noPol;
+        this.kapasitas = kapasitas;
+        this.hargaSewa = hargaSewa;
         this.statusKendaraan = statusKendaraan;
     }
-    public String getNoPol() {
+
+    public String getJenisKendaraan(){
+        return jenisKendaraan;
+    }
+    public String getNoPol(){
         return noPol;
     }
-    public void setNoPol(String noPol) {
-        this.noPol = noPol;
-    }
-    public int getKapasitas() {
+    public int getKapasitas(){
         return kapasitas;
     }
-    public void setKapasitas(int kapasitas) {
-        this.kapasitas = kapasitas;
+
+    public double getHargaSewa(){
+        return hargaSewa;
     }
+    public void informasiKendaraan(){
+        System.out.println("Jenis kendaraan: " + jenisKendaraan);
+        System.out.println("Plat nomor: " + noPol);
+        System.out.println("Kapasitas: " + kapasitas);
+        System.out.println("Harga sewa: " + hargaSewa);
+        System.out.println("Status kendaraan: " +(statusKendaraan ? "Tersedia" : "Tidak Tersedia"));
+    }
+
 }
