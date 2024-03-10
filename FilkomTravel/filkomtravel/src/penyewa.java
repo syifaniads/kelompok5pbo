320eaa986070098eb7ef81be08efd22ffd5fc2b6
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-03-10 21:33:16 +0700

    Update penyewa.java

--- FilkomTravel/filkomtravel/src/penyewa.java
+++ FilkomTravel/filkomtravel/src/penyewa.java
@@ -1,4 +1,7 @@
+package studiKasus1;
+import java.util.*;
 public class Penyewa extends user {
+    Scanner scanner = new Scanner(System.in);
     private String nama;
     private String noHP;
     private String alamat;
@@ -6,16 +9,28 @@ public class Penyewa extends user {
     private double hargaSewa;
     private String jenisKendaraan;
 
+
     public void setHargaSewa(double hargaSewa) {
         this.hargaSewa = hargaSewa;
     }
+
     public void setNoPol(String noPol) {
         this.noPol = noPol;
     }
+
     public void setStatusKendaraan(boolean status) {
         // implementasi
     }
+
     public void setJenisKendaraan(String jenisKendaraan) {
         this.jenisKendaraan = jenisKendaraan;
+    }
+    public void menyewa(){
+        System.out.print("Masukkan nama: ");
+        String namaPenyewa = scanner.nextLine();
+        System.out.print("Masukkan Nomor Telepon: ");
+        String noTelpPenyewa = scanner.nextLine();
+        System.out.print("Masukkan Alamat: ");
+        String alamatPenyewa = scanner.nextLine();
     }
 }
