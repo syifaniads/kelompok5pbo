75d46d78768f3b89f93c96625ac40e5d3be1113e
syifaniads <148206758+syifaniads@users.noreply.github.com>
2024-03-10 22:44:29 +0700

    Update nota.java

--- FilkomTravel/filkomtravel/src/nota.java
+++ FilkomTravel/filkomtravel/src/nota.java
@@ -4,9 +4,25 @@ public class Nota {
     private int lamaSewa;
     private double totalBiaya;
     private int voucherMember;
+    private double totalBiayaSetelahDiskon;
 
-    public void cetakNota() {
-        // implementasi
+    public void cetakNota() { 
+        if (member) {
+            System.out.println("=".repeat(52));
+            System.out.println("Nama Pelanggan: " + namaPelanggan);
+            System.out.println("Jenis: " + jenis);
+            System.out.println("Lama Sewa: " + lamaSewa + " hari");
+            totalBiayaSetelahDiskon = totalBiaya - (totalBiaya * 0.2); 
+            System.out.println("=".repeat(52));// Memberikan diskon 20%
+        } else {
+            totalBiayaSetelahDiskon = totalBiaya; // Tidak ada diskon untuk non-member
+        }
+        System.out.println("=".repeat(52));
+        System.out.println("Nama Pelanggan: " + namaPelanggan);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Lama Sewa: " + lamaSewa + " hari");
+        System.out.println("Total Biaya: Rp " + totalBiayaSetelahDiskon);
+        System.out.println("=".repeat(52)); // Menampilkan total biaya setelah diskon
     }
     public double getTotalBiaya() {
         return this.totalBiaya;
