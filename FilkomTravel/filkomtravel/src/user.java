ae5716065f028c11d662c549ef1c7ad32f6c8fa0
mauliasugeha <162555141+mauliasugeha@users.noreply.github.com>
2024-03-20 16:47:32 +0700

    user.java

--- FilkomTravel/filkomtravel/src/user.java
+++ FilkomTravel/filkomtravel/src/user.java
@@ -1,9 +1,30 @@
 package studiKasus1;
 import java.util.*;
-public class user {
-    String nama;
-    String noHP;
-    String alamat;
+import java.util.Scanner;
+
+public class User {
+    Scanner scanner = new Scanner(System.in);
+    protected String nama;
+    protected String noHP;
+    protected String alamat;
+    protected String tanggalAwalSewa;
+    protected String tanggalAkhirSewa;
+    protected int durasiSewa;
+    protected double hargaSewa;
+//    protected Object buatPesanan;
+//
+//    protected Object editPesanan;
+//    protected Object batalkanPesanan;
+
+    public void setTanggalAwalSewa(String tanggalAwalSewa){
+         this.tanggalAwalSewa = tanggalAwalSewa;
+    }
+    public void setTanggalAkhirSewa(String tanggalAkhirSewa){
+        this.tanggalAkhirSewa = tanggalAkhirSewa;
+    }
+    public void setDurasiSewa(int durasiSewa) {
+        this.durasiSewa = durasiSewa;
+    }
     public void setNama(String s) {
         this.nama = s;
     }
@@ -13,164 +34,44 @@ public class user {
     public void setAlamat(String s) {
         this.alamat = s;
     }
-//    user member = new user();
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        System.out.print("=".repeat(52));
-        System.out.print("\n"+"=".repeat(15));
-        System.out.print("APLIKASI FILKOM TRAVEL");
-        System.out.print("=".repeat(15));
-        System.out.print("\n"+"=".repeat(52));
-        System.out.print("\nSIAPA ANDA?");
-        System.out.print("\nKetik 1 Daftar Member");
-        System.out.print("\nKetik 2 Daftar non-Member");
-        System.out.print("\nPILIH MASUKKAN: ");
-        int choice = scanner.nextInt();
+    public void daftar(){
+        User user = new User();
+        System.out.print("Masukkan Nama: ");
+        user.setNama(scanner.nextLine());
+        System.out.print("Masukkan Nomor Telepon: ");
+        user.setNoHP(scanner.nextLine());
+        System.out.print("Masukan Alamat: ");
+        user.setAlamat(scanner.nextLine());
+    }
+    public void buatPesanan() {
+        User user = new User();
+        System.out.println("================ Buat Pesanan ================");
+        System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
+        user.setTanggalAwalSewa(scanner.nextLine());
+        System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
+        user.setTanggalAkhirSewa(scanner.nextLine());
+        System.out.print("Durasi penyewaan (dalam hari): ");
+        user.setDurasiSewa(scanner.nextInt());
         scanner.nextLine();
-        switch(choice){
-            case 1:
-                Member member = new Member();
-                member.jadiMember();
-                System.out.print("=".repeat(52));
-                System.out.println("\nLANJUTKAN PESANAN?");
-                System.out.println("1. Ya");
-                System.out.println("2. Tidak");
-                System.out.print("Masukkan pilihan: ");
-                int pilihan1 = scanner.nextInt();
-                scanner.nextLine();
-                if(pilihan1==1){
-                    Member pesanMember = new Member();
-                    pesanMember.buatPesanan();
-                    System.out.println("Apakah Pesanan Sudah Tepat?");
-                    System.out.println("1. Ya");
-                    System.out.println("2. Tidak,Saya Ingin Mengedit Pesanan");
-                    System.out.println("3. Batalkan Pesanan");
-                    System.out.print("Masukkan pilihan: ");
-                    int pilihan2 = scanner.nextInt();
-                    if(pilihan2 == 2){
-                        pesanMember.editPesanan();
-                        System.out.println("Pesanan berhasil diedit");
-                    }
-                    if(pilihan2 == 3){
-                        pesanMember.batalkanPesanan();
-                        System.out.println("Pesanan berhadil dibatalkan");
-                    }
-                    System.out.println("Apa yang Anda inginkan?");
-                    System.out.println("1. Menyewa");
-                    System.out.println("2. Memesan Travel");
-                    System.out.print("Masukkan pilihan anda: ");
-                    int pilihan3 = scanner.nextInt();
-                    if(pilihan3 == 1){
-                        System.out.println("Berikut adalah  daftar kendaraan sewaan: ");
-                        mobil mobil = new mobil("Mobil", "N 1234 AAA", 5, 800000, true, "Mercedes-Benz", "E-Class", true);
-                        mobil.informasiKendaraan();
-                        bus bus = new bus("Bus", "N 1234 AAA", 40, 35000000, true, "Scania", "K-Series", true);
-                        bus.informasiKendaraan();
-                        motor motor = new motor("Motor", "N 1234 AAA", 2, 125000, true, "Honda", "Vario");
-                        motor.informasiKendaraan();
-                        System.out.println("Kendaran apa yang ingin ada sewa?");
-                        String pilihKendaraan = scanner.nextLine();
-                        if(pilihKendaraan.equals("Mobil")){
-                            Nota mobilNota = new Nota();
-                            mobilNota.cetakNotaMember(800000);
-                        }
-                        if(pilihKendaraan.equals("Bus")){
-                            Nota busNota = new Nota();
-                            busNota.cetakNotaMember(3500000);
-                        }
-                        if(pilihKendaraan.equals("Motor")){
-                            Nota motorNota = new Nota();
-                            motorNota.cetakNotaMember(125000);
-                        }
-                        else{
-                            System.out.println("Maaf kendaraan tersebut tidak tersedia");
-                        }
-                    }
-                    if(pilihan3 == 2){
-                        paketTravel travel = new paketTravel("Bromo", 3, 1000000);
-                        travel.printPaketTravel();
-                        Nota travelNota = new Nota();
-                        travelNota.cetakNotaMember(1000000);
-                    }
-                }
-                else{
-                    System.out.println("Terimakasih!");
-                    break;
-                }
-                break;
-            case 2:
-                NonMember nonMember = new NonMember();
-                nonMember.daftarNonMember();
-                System.out.print("=".repeat(52));
-                System.out.println("\nLANJUTKAN PESANAN?");
-                System.out.println("1. Ya");
-                System.out.println("2. Tidak");
-                System.out.print("Masukkan pilihan: ");
-                int pilihan4 = scanner.nextInt();
-                scanner.nextLine();
-                if(pilihan4==1){
-                    NonMember pesanNonMember = new NonMember();
-                    pesanNonMember.buatPesanan();
-                    System.out.println("Apakah Pesanan Sudah Tepat?");
-                    System.out.println("1. Ya");
-                    System.out.println("2. Tidak,Saya Ingin Mengedit Pesanan");
-                    System.out.println("3. Batalkan Pesanan");
-                    System.out.print("Masukkan pilihan: ");
-                    int pilihan5 = scanner.nextInt();
-                    if(pilihan5 == 2){
-                        pesanNonMember.editPesanan();
-                        System.out.println("Pesanan berhasil diedit");
-                    }
-                    if(pilihan5 == 3){
-                        pesanNonMember.batalkanPesanan();
-                        System.out.println("Pesanan berhadil dibatalkan");
-                    }
-                    System.out.println("Apa yang Anda inginkan?");
-                    System.out.println("1. Menyewa");
-                    System.out.println("2. Memesan Travel");
-                    System.out.print("Masukkan pilihan anda");
-                    int pilihan6 = scanner.nextInt();
-                    if(pilihan6 == 1){
-                        System.out.println("Berikut adalah  daftar kendaraan sewaan: ");
-                        mobil mobil = new mobil("Mobil", "N 1234 AAA", 5, 1500000, true, "Mercedes-Benz", "E-Class", true);
-                        mobil.informasiKendaraan();
-                        bus bus = new bus("Bus", "N 1234 AAA", 40, 35000000, true, "Scania", "K-Series", true);
-                        bus.informasiKendaraan();
-                        motor motor = new motor("Motor", "N 1234 AAA", 2, 125000, true, "Honda", "Vario");
-                        motor.informasiKendaraan();
-                        System.out.println("Kendaran apa yang ingin ada sewa?");
-                        String pilihKendaraan = scanner.nextLine();
-                        if(pilihKendaraan.equals("Mobil")){
-                            Nota mobilNota = new Nota();
-                            mobilNota.cetakNotaNonMember(800000);
-                        }
-                        if(pilihKendaraan.equals("Bus")){
-                            Nota busNota = new Nota();
-                            busNota.cetakNotaNonMember(3500000);
-                        }
-                        if(pilihKendaraan.equals("Motor")){
-                            Nota motorNota = new Nota();
-                            motorNota.cetakNotaNonMember(125000);
-                        }
-                        else{
-                            System.out.println("Maaf kendaraan tersebut tidak tersedia");
-                        }
-                    }
-                    if(pilihan6 == 2){
-                        paketTravel travel = new paketTravel("Bromo", 3, 1000000);
-                        travel.printPaketTravel();
-                        Nota travelNota = new Nota();
-                        travelNota.cetakNotaMember(1000000);
-                    }
-                }
-                else{
-                    System.out.println("Terimakasih!");
-                    break;
-                }
-                break;
-            default:
-                System.out.println("Pilihan tidak valid.");
-                break;
-        }
+    }
+
+    public void editPesanan() {
+        User user = new User();
+        System.out.println("Edit Pesanan:");
+        System.out.print("Tanggal awal sewa (dd-mm-yyyy): ");
+        user.setTanggalAwalSewa(scanner.nextLine());
+        System.out.print("Tanggal akhir sewa (dd-mm-yyyy): ");
+        user.setTanggalAkhirSewa(scanner.nextLine());
+        System.out.print("Durasi penyewaan (dalam hari): ");
+        user.setDurasiSewa(scanner.nextInt());
+        scanner.nextLine();
+    }
+
+    public void batalkanPesanan() {
+        System.out.println("Batalkan Pesanan:");
+        tanggalAwalSewa = null;
+        tanggalAkhirSewa = null;
+        durasiSewa = 0;
+        hargaSewa = 0.0;
     }
 }
